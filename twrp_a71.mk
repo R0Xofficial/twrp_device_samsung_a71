49dce9eb939ac7a88875a7fd183f825aee71aa22
R0X <patryk.bieganski1003@gmail.com>
2025-03-27 21:38:51 +0100

    Update twrp_a71.mk

--- twrp_a71.mk
+++ twrp_a71.mk
@@ -19,6 +19,10 @@ PRODUCT_RELEASE_NAME := a71
 
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
