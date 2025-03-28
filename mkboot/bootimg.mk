161c9c843dbf3d578bbd5b6ffe8e8e9a2a00aa92
R0X <patryk.bieganski1003@gmail.com>
2025-03-28 08:14:50 +0100

    Update bootimg.mk

--- mkboot/bootimg.mk
+++ mkboot/bootimg.mk
@@ -1,7 +1,3 @@
-MKBOOTIMG := device/samsung/a71/mkbootimg
-
-FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
-
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS)
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) --kernel $(call bootimage-to-kernel,$(1)) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
